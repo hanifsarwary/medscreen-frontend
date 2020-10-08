d85eb4dd56f62583d556e96298d3e90594d18975
Asma Noreen <asmaaneeza@addo.ai>
2020-10-08 15:31:27 +0500

    fixed issues

--- src/pages/appointments/appointmentHistory/appointmentHistory.jsx
+++ src/pages/appointments/appointmentHistory/appointmentHistory.jsx
@@ -4,7 +4,6 @@ import { Button, Accordion, Card } from 'react-bootstrap';
 
 export default class AppointmentHistory extends Component {
     render() {
-        console.log(this.props.past_appointments);
         return (
             <Accordion>
             {
