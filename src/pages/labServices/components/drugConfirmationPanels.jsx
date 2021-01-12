a5b53f0296da8a73afc6ceb3acf831abcbc607a0
asmaaneeza <asmaaneeza@addo.ai>
2021-01-12 22:15:24 +0500

    Responsive issues has been resolved.

--- src/pages/labServices/components/drugConfirmationPanels.jsx
+++ src/pages/labServices/components/drugConfirmationPanels.jsx
@@ -2,58 +2,56 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import beaker from 'assets/images/icons/icon2.svg';
 
-
 export default function DrugConfirmationPanels(props) {
-    return (
-      <div class="dark-wrapper" id="drug-confirmation">
-           { props.chlidCategory.map((category, i) => {
-             return (
-              <div class="container inner" key={i}>
-                <div class="section-title text-center">
-                  <h2>{category.name}</h2>
-                  {/* <h3>We are here to serve you</h3> */}
-                  <div className="description container">
-                    <p class="wow fadeIn" data-wow-duration="3s" data-wow-delay={0.5 + 's'} style={{fontSize: '16px'}} dangerouslySetInnerHTML={{ __html: category.description }}>
-                        {/* {category.description} */}
-                    </p>
-                  </div>
-                </div>
-                <div class="row" key={i}>
-                  {
-                    category.panel.map((panel, i) => {
-                      return (
-                        <div class="col-sm-4 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.3s" key={i}>
-                          <div class="row" style={{display: 'flex'}}>
-                            <div class="col-sm-4 icon icon-m">
-                              <img src={beaker} alt="" />
-                            </div>
-                            <div class="col-sm-8 pl-0">
-                              <h4 className="tm30 text-left">{panel.panel_name}</h4>
-                              <ul>
-                                {
-                                  panel.tests.map((test, i) => {
-                                    return (
-                                      <li key={i}>{test.title}</li>
-                                    )
-                                  })
-                                }
-                            </ul>
-                            </div>
-                          </div>
-                        </div>
-                      )
-                    })
-                  }
-                </div>
-                <div className="btn-appointments tp20">
-                  <Link to="/appointments" class="btn btn-large">
-                              Book an Appointment
-                  </Link>
-                </div>
+  return (
+    <div class="dark-wrapper" id="drug-confirmation">
+      {props.chlidCategory.map((category, i) => {
+        return (
+          <div class="container inner" key={i}>
+            <div class="section-title text-center">
+              <h2>{category.name}</h2>
+              {/* <h3>We are here to serve you</h3> */}
+              <div className="description container">
+                <p
+                  class="wow fadeIn"
+                  data-wow-duration="3s"
+                  data-wow-delay={0.5 + 's'}
+                  style={{ fontSize: '16px' }}
+                  dangerouslySetInnerHTML={{ __html: category.description }}
+                >
+                  {/* {category.description} */}
+                </p>
               </div>
-              )
-            })
-           }
-      </div>
-    );
+            </div>
+            <div class="row" key={i}>
+              {category.panel.map((panel, i) => {
+                return (
+                  <div class="col-sm-4 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.3s" key={i}>
+                    <div class="row" style={{ display: 'flex' }}>
+                      <div class="col-sm-4 icon ">
+                        <img src={beaker} alt="" className="responsive" />
+                      </div>
+                      <div class="col-sm-8 pl-0">
+                        <h4 className="tm30 text-left">{panel.panel_name}</h4>
+                        <ul>
+                          {panel.tests.map((test, i) => {
+                            return <li key={i}>{test.title}</li>;
+                          })}
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="btn-appointments tp20">
+              <Link to="/appointments" class="btn btn-large">
+                Book an Appointment
+              </Link>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
--- src/pages/labServices/components/drugScreeningPanels.jsx
+++ src/pages/labServices/components/drugScreeningPanels.jsx
@@ -2,49 +2,45 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import beaker from 'assets/images/icons/icon2.svg';
 
-
 export default function DrugScreeningPanels(props) {
-    return (
-      <div class="dark-wrapper">
-        { props.panel.length > 0 ?
-            <div class="container inner">
-            <div class="section-title text-center">
-              {/* <h2>Drug Screening</h2> */}
-              {/* <h3>We are here to serve you</h3> */}
-            </div>
-            <div class="row">
-              { props.panel.map((panel, i) => {
-                  return (
-                    <div key={i} class="col-sm-4 col-md-4 wow fadeIn" data-wow-duration="1.5s" data-wow-delay="0.5s">
-                      <div className="row">
-                          <div class="icon icon-m col-sm-4">
-                            <img src={beaker} alt="" />
-                          </div>
-                          <div class="col-sm-8 pl-0">
-                          <h4 className="tm30">{panel.panel_name}</h4>
-                            <ul>
-                                {
-                                  panel.tests.map((test, i) => {
-                                    return (
-                                      <li key={i}>{test.title}</li>
-                                    )
-                                  })
-                                }
-                            </ul>
-                          </div>
-                      </div>
+  return (
+    <div class="dark-wrapper">
+      {props.panel.length > 0 ? (
+        <div class="container inner">
+          <div class="section-title text-center">
+            {/* <h2>Drug Screening</h2> */}
+            {/* <h3>We are here to serve you</h3> */}
+          </div>
+          <div class="row">
+            {props.panel.map((panel, i) => {
+              return (
+                <div key={i} class="col-sm-4 col-md-4 wow fadeIn" data-wow-duration="1.5s" data-wow-delay="0.5s">
+                  <div className="row">
+                    <div class="icon col-sm-4">
+                      <img src={beaker} alt="" className="responsive" />
                     </div>
-                  )
-                })
-              }
-            </div>
-            <div className="btn-appointments tp20">
-                <Link to="/appointments" class="btn btn-large">
-                            Book an Appointment
-                </Link>
-            </div>
-          </div> : ''
-        }
-      </div>
-    );
+                    <div class="col-sm-8 pl-0">
+                      <h4 className="tm30">{panel.panel_name}</h4>
+                      <ul>
+                        {panel.tests.map((test, i) => {
+                          return <li key={i}>{test.title}</li>;
+                        })}
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="btn-appointments tp20">
+            <Link to="/appointments" class="btn btn-large">
+              Book an Appointment
+            </Link>
+          </div>
+        </div>
+      ) : (
+        ''
+      )}
+    </div>
+  );
 }
