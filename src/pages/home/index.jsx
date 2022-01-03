65cdf0b7d107535f5158a2a29dcb95b94f08aba3
aziz-ahmed-khan <aziz.ahmed@swyftlogistics.com>
2022-01-04 01:27:59 +0500

    laptop modal updated

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -55,8 +55,8 @@ class HomePage extends Component {
               },
               content: {
                 position: 'absolute',
-                height: '35%',
-                width: '40%',
+                height: '40%',
+                width: '50%',
                 left: '50%',
                 top: '50%',
                 transform: 'translate(-50%, -50%)',
